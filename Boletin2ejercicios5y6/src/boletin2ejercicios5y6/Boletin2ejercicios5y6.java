579dc02ad49144ec3fea7341c60f76df24afe798
aabalalonso <aabalalonso@atanasoff15.danielcastelao.org>
2015-10-15 09:29:12 +0200

    commit de proyecto alfa 2

---
+++ Boletin2ejercicios5y6/src/boletin2ejercicios5y6/Boletin2ejercicios5y6.java
@@ -0,0 +1,29 @@
+
+package boletin2ejercicios5y6;
+
+import java.util.Scanner;
+
+
+public class Boletin2ejercicios5y6 {
+static final int valor= 1852;
+    
+    public static void main(String[] args) {
+        
+        float millas;
+        float metros ;
+        Scanner obx= new Scanner(System.in);
+        System.out.println("Escribe las millas ");
+        millas=obx.nextFloat();
+        metros= millas*valor;
+        System.out.println("El resultado en metros son :"+metros);
+        
+        //Ejercicio 6
+        
+        //cambio para subir a git hub
+        
+        
+        
+        
+    }
+    
+}
