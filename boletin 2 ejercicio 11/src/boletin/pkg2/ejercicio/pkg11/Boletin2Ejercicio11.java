d232ee46acb5c2a6a1269ad9b7e7a3105361b98a
aabalalonso <aabalalonso@atanasoff15.danielcastelao.org>
2015-10-15 09:38:00 +0200

    commit de proyecto alfa 2

---
+++ boletin 2 ejercicio 11/src/boletin/pkg2/ejercicio/pkg11/Boletin2Ejercicio11.java
@@ -0,0 +1,23 @@
+
+package boletin.pkg2.ejercicio.pkg11;
+
+import java.util.Scanner;
+
+
+public class Boletin2Ejercicio11 {
+
+    
+    public static void main(String[] args) {
+        float sueldofijo , diasdesplazamiento , kmRecorridos , Ventas ;
+        Scanner obx = new Scanner (System.in);
+        System.out.println("Introduce el sueldo fijo :");
+        sueldofijo = obx.nextFloat();
+        System.out.println("Introduce las ventas :");
+        Ventas = obx.nextFloat();
+        System.out.println("Introduce ");
+        
+        
+        
+    }
+    
+}
